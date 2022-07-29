d411144627622a7ffa6ee85551e4f4d63d9bab16
kriti-dhingra <76401681+kriti-dhingra@users.noreply.github.com>
2022-07-29 18:32:38 +0530

    API validation is done in the controller.

--- src/main/java/com/register_login/reg_login_page/model/User.java
+++ src/main/java/com/register_login/reg_login_page/model/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class User {
@@ -29,18 +32,22 @@ public class User {
 	public void setId(int id) {
 		this.id = id;
 	}
+	@NotBlank(message = "Email is required")
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	@NotBlank(message = "Password is required")
+	@Length(min = 6, message = "Password must be atleast 6 characters long")
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@NotBlank(message = "Phone number is required")
 	public int getPhno() {
 		return phno;
 	}
