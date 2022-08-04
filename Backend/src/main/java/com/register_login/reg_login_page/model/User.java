a150cda8ff3d1c0291d87db07429ada88aa5eb7f
kriti-dhingra <76401681+kriti-dhingra@users.noreply.github.com>
2022-08-04 14:52:50 +0530

    API validation

--- Backend/src/main/java/com/register_login/reg_login_page/model/User.java
+++ Backend/src/main/java/com/register_login/reg_login_page/model/User.java
@@ -1,6 +1,8 @@
 package com.register_login.reg_login_page.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Entity
 public class User {
@@ -19,24 +21,34 @@ public class User {
 		// this.id=id;
 	}
 	
+	@NotBlank(message = "Email is required")
+	@Email(message = "Email is not valid")
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	@NotBlank(message = "Password is required")
+	@Size(min = 6, message = "Password must be at least 6 characters")
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@NotBlank(message = "User Name is required")
 	public String getUser_name() {
 		return this.user_name;
 	}
+
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
+
+	@NotBlank(message = "Phone Number is required")
+	@Size(min = 10, max = 10, message = "Phone Number must be 10 digits")
 	public long getPhno(){
 		return this.phno;
 	}
